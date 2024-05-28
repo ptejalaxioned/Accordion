276092213ad26372dae90424fc56e5ad95c577c3
ptejalaxioned <tejalp@axioned.com>
2024-05-28 19:43:09 +0530

    main Branch Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,66 +2,34 @@
 
 */
 
-let accordion_up=document.querySelector(".accordion-up")
-let accordion_down=document.querySelector(".accordion-down")
-let li=document.querySelector("li")
-let ul=document.querySelector("ul")
+let accordion_up = document.querySelector(".accordion-up");
+let accordion_down = document.querySelector(".accordion-down");
+let li = document.querySelector("li");
+let ul = document.querySelector("ul");
 
-ul.children[0].classList.toggle("first")
+ul.children[0].classList.toggle("first");
 
+let button = document.querySelectorAll(".plus");
+button.forEach((val, index) => {
+  button[index].addEventListener("click", (e) => {
+    let a = e.target.parentElement.parentElement.children[1];
+    let b = e.target.parentElement.parentElement;
+    if (b.className === "first") {
+      b.classList.toggle("first");
+      if (button[index].parentElement.parentElement.className != "first") {
+        button[0].parentElement.parentElement.classList.toggle("first");
+      }
+    } else {
+      b.classList.toggle("first");
+      for (let i = 0; i < button.length; i++) {
+        if (index == i) {
+          continue;
+        }
 
-let button=document.querySelectorAll(".plus")
-button.forEach((val,index)=> {
-   
-    button[index].addEventListener("click",(e)=>{
-        let a=e.target.parentElement.parentElement.children[1]
-        let b=e.target.parentElement.parentElement
-        if(b.className==="first")
-            {
-                b.classList.toggle("first")
-                if(button[index].parentElement.parentElement.className!="first")
-                    {
-                        button[0].parentElement.parentElement.classList.toggle("first")
-                    }
-            }
-            else{
-                b.classList.toggle("first")
-                for(let i=0;i<button.length;i++){
-                    if(index==i){
-                        continue;
-                    }
-                    console.log(button[i].parentElement.parentElement ,i)
-        
-                    if(button[i].parentElement.parentElement.className=="first")
-                        {
-                            button[i].parentElement.parentElement.removeAttribute("class")
-                        }
-                      
-                    
-                }
-           
-            }
-            
-
-    })
+        if (button[i].parentElement.parentElement.className == "first") {
+          button[i].parentElement.parentElement.removeAttribute("class");
+        }
+      }
+    }
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
